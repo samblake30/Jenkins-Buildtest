9daf19319d9aec07b72e038882e9c442cb5a2b21
Ankit Sambhare <ankitsambhare3004@gmail.com>
2020-12-05 18:13:37 +0530

    better made

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 echo "This script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
-echo "$NUMBER"
-echo "This is added"
-echo "this is added 2nd time"
+echo "The addition number is : $NUMBER"
